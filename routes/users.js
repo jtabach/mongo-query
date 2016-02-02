c839ae5254751447befc9bfe9d85cf0ec45c8a2e
jtabach <jeff.n.tabachnick@gmail.com>
2016-02-01 16:38:49 -0800

    query for sort and limit

--- routes/users.js
+++ routes/users.js
@@ -6,17 +6,33 @@ var Person = require('../models/user');
 router.post('/add', function(req, res) {
 	Person.add(req.body, function(err, savedUser) {
 		res.status(err ? 400 : 200).send(err || savedUser);
-	})
-})
+	});
+});
 
 
 
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  Person.find({}, function(err, users) {
-  	res.status(err ? 400 : 200).send(err || users);
-  })
+	console.log(req.query);
+
+	if (req.query.sort) {
+		var sortObj = {};
+		sortObj[req.query.sort] = req.query.desc ? -1 : 1;
+	}
+
+	if (req.query.limit) {
+		var limit = parseInt(req.query.limit);
+	}
+
+	delete req.query.sort;
+	delete req.query.desc;
+	delete req.query.limit;
+
+  	Person.find(req.query).limit(limit).sort(sortObj)
+  	.exec(function(err, users) {
+  		res.status(err ? 400 : 200).send(err || users);
+  	});
 });
 
 router.get('/over60', function(req, res, next) {
